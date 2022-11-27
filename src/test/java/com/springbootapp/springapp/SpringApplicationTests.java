216a1e2f68f7e5d8f562079dcc36c8112eb53707
guptagaurav288 <guptagaurav288@gmail.com>
2022-11-27 16:22:58 +0530

    api development conf

---
+++ src/test/java/com/springbootapp/springapp/SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapp.springapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.testng.annotations.Test;
+
+@SpringBootApplication
+class SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
